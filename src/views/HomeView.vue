5185432c43a0ffe33391f6111aa2c8cd1a528694
CTS SURAJ <suraj.gurav.cerisetechsolution@gmail.com>
2025-06-06 10:07:12 +0530

    changes in Homeview message Cerisetech Solutions pvt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Cerisetech Solutions"/>
+    <HelloWorld msg="Welcome to Cerisetech Solutions PVT"/>
   </div>
 </template>
 
